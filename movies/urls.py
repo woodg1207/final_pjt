1f65f10134302e1cddaf5994ea3dfc93c8408c30
woo <woodg1207@gmail.com>
2019-11-22 16:08:54 +0900

    comit

--- movies/urls.py
+++ movies/urls.py
@@ -6,4 +6,5 @@ app_name='movies'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:movie_pk>', views.detail, name='detail'),
+    path('<int:movie_pk>/reviews/', views.review_create, name='review_create'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Movie, Genre
+from .models import Movie, Genre, Review
+from .forms import ReviewForm
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 
 # Create your views here.
+@login_required
 def index(request):
     movies = Movie.objects.all()
     context = {'movies': movies,}
@@ -9,5 +14,23 @@ def index(request):
 
 def detail(request, movie_pk):
     movie = get_object_or_404(Movie, pk=movie_pk)
-    context = {'movie':movie,}
+    reviews = movie.review_set.all()
+    reviewform = ReviewForm()
+    context = {'movie':movie, 'reviewform':reviewform, 'reviews': reviews}
     return render(request, 'movies/detail.html', context)
+
+@require_POST
+def review_create(request, movie_pk):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                review = form.save(commit=False)
+                review.movie_id=movie_pk
+                review.user = request.user
+                review.save()
+                return redirect('movies:detail', movie_pk)
+    return redirect('movies:detail', movie_pk)
+
+# @login_required
+# def review_delete(request, movie)
