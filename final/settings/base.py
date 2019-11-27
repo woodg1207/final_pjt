b1e9deaf14b937ec80391ce45ffef877715e295b
baambox5 <baamboxo@gmail.com>
2019-11-27 15:39:30 +0900

    third deploy commit

--- final/settings/base.py
+++ final/settings/base.py
@@ -125,13 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"), # Root의 static 파일
-    os.path.join(BASE_DIR, 'movies', 'static'),    # movies App의 static 파일
-    os.path.join(BASE_DIR, 'accounts', 'static'),
-)
+STATIC_ROOT = 'static'
 
 AUTH_USER_MODEL = 'accounts.User'
 
